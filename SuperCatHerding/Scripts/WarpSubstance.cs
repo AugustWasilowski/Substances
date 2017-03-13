1ee8afee8797041cd2eaa9a57896a654c33a8fb7
August Wasilowski <august.wasilowski@gmail.com>
2017-03-12 22:13:24 -0500

    Added GooBallAnimated video and script

---
+++ SuperCatHerding/Scripts/WarpSubstance.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WarpSubstance : MonoBehaviour
+{
+    public Renderer rend;
+    public float cycleTime = 10;
+    public string floatRangeProperty = "Warp_Intensity";
+	void Start ()
+    {
+        rend = GetComponent<Renderer>();
+	}
+	
+
+	void Update ()
+    {
+        ProceduralMaterial substance = rend.sharedMaterial as ProceduralMaterial;
+
+        if (substance)
+        {
+            float lerp = Mathf.PingPong(Time.time * 2 / cycleTime, 1);
+            substance.SetProceduralFloat("Warp_Intensity", lerp);
+            substance.RebuildTextures();
+            
+        }
+	}
+}
